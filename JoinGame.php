f1889e1a1498e0333b9becf82eec55fc3cda8422
Hardmgamer <redmohammad22@gmail.com>
2020-04-25 21:30:23 +0200

    Minor bugs

--- JoinGame.php
+++ JoinGame.php
@@ -1,4 +1,4 @@
 <?php
-$home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php?invite='.$_GET['code'];
-header('Location:'.$home_url);
+$GamrURL = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php?invite='.$_GET['code'];
+header('Location:'.$GamrURL);
 ?>
