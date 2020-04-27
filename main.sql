726fe355d9d78746802f2380f9e111b22b75d884
Hardmgamer <redmohammad22@gmail.com>
2020-04-27 21:32:59 +0200

    Updating SQL

--- main.sql
+++ main.sql
@@ -1,62 +1,124 @@
-/*
-SQLyog Professional v13.1.1 (64 bit)
-MySQL - 10.4.8-MariaDB : Database - bingo
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2020 at 09:31 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`bingo` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `bingo`;
+--
+-- Database: `bingo`
+--
 
-/*Table structure for table `codesrepo` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `codesrepo`;
+--
+-- Table structure for table `codesrepo`
+--
 
 CREATE TABLE `codesrepo` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `game` int(11) unsigned NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL,
+  `game` int(11) UNSIGNED NOT NULL,
   `code` char(10) DEFAULT NULL,
   `username` text DEFAULT NULL,
-  `checked` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `gameid` (`game`),
-  CONSTRAINT `codesrepo_ibfk_1` FOREIGN KEY (`game`) REFERENCES `games` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1652 DEFAULT CHARSET=utf8mb4;
+  `checked` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Table structure for table `games` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `games`;
+--
+-- Table structure for table `games`
+--
 
 CREATE TABLE `games` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL,
   `player1` text DEFAULT NULL,
   `player2` text DEFAULT NULL,
-  `status` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8mb4;
+  `status` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Table structure for table `turnlog` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `turnlog`;
+--
+-- Table structure for table `turnlog`
+--
 
 CREATE TABLE `turnlog` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL,
   `gameid` int(11) DEFAULT NULL,
   `player` text DEFAULT NULL,
   `number` int(3) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=112 DEFAULT CHARSET=utf8mb4;
+  `status` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `codesrepo`
+--
+ALTER TABLE `codesrepo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `gameid` (`game`);
+
+--
+-- Indexes for table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `turnlog`
+--
+ALTER TABLE `turnlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `codesrepo`
+--
+ALTER TABLE `codesrepo`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `turnlog`
+--
+ALTER TABLE `turnlog`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `codesrepo`
+--
+ALTER TABLE `codesrepo`
+  ADD CONSTRAINT `codesrepo_ibfk_1` FOREIGN KEY (`game`) REFERENCES `games` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
